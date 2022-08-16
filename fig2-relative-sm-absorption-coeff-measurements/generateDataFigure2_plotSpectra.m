92a6c7fb5faf0e7e0b82aad878991302f5ab1a1b
Ezra Bruggeman <42326863+ezrabru@users.noreply.github.com>
2022-08-16 14:11:34 +0100

    Edited script for plotting spectra of 2 lobe experiment

--- fig2-relative-sm-absorption-coeff-measurements/generateDataFigure2_plotSpectra.m
+++ fig2-relative-sm-absorption-coeff-measurements/generateDataFigure2_plotSpectra.m
@@ -16,6 +16,11 @@ col_red = 1.05*[0.8392,0.1529,0.1569]; % red
 % col = [0.5490,0.3373,0.2941];
 % col = [0.8902,0.4667,0.7608];
 
+col_405 = [135 90 175]/255; % 405 nm
+col_488 = [2 100 173]/255; % 488 nm
+col_561 = [177 212 55]/255; % 561 nm
+col_638 = [212 0 0]/255; % 638 nm
+
 qdot585 = readmatrix(fullfile(pwd,'spectra dyes and filters','dyes','Qdot585.txt'));
 T_qdot585.wavelength = qdot585(:,1);
 T_qdot585.ex = qdot585(:,2)/100;
@@ -42,20 +47,20 @@ qe = readmatrix(fullfile(pwd,'spectra dyes and filters','filters','imagEM_C9100-
 T_qe.wavelength = qe(:,1);
 T_qe.qe = qe(:,2)/100;
 
-%% Plot data
+%% Plot data light mode
 
 fig = figure('Position',[600 300 850 200]);
 subplot(1,2,1)
-line([405 405],[0 1],'LineWidth',2); hold on
-line([488 488],[0 1],'LineWidth',2);
-line([561 561],[0 1],'LineWidth',2);
-line([638 638],[0 1],'LineWidth',2);
+line([405 405],[0 1],'LineWidth',2,'Color',col_405); hold on
+line([488 488],[0 1],'LineWidth',2,'Color',col_488);
+line([561 561],[0 1],'LineWidth',2,'Color',col_561);
+line([638 638],[0 1],'LineWidth',2,'Color',col_638);
 plot(T_qdot585.wavelength,T_qdot585.ex/0.48,'Color',col_orange,'LineWidth',2);
 plot(T_exfilter.wavelength,T_exfilter.transmission,'k');
 plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',col_lightgray);
 xlim([350 750]); ylim([0 1]); xticks(300:50:800)
-xlabel('Wavelength (nm)')
-set(gca,'FontSize',fontsize)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'Layer','top')
 
 subplot(1,2,2)
 plot(T_qdot585.wavelength,T_qdot585.em,'Color',col_orange,'LineWidth',2); hold on
@@ -63,8 +68,8 @@ plot(T_emfilter.wavelength,T_emfilter.transmission,'k');
 plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',col_lightgray);
 plot(T_qe.wavelength,T_qe.qe,'--k');
 xlim([350 750]); ylim([0 1]); xticks(300:50:800)
-xlabel('Wavelength (nm)')
-set(gca,'FontSize',fontsize)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'Layer','top')
 
 savefig(fig,fullfile(directory_out,'experimentSpectra_QDot585_lightMode.fig'))
 exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot585_lightMode.png'),'Resolution',400)
@@ -74,16 +79,16 @@ exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot585_lightMode.e
 
 fig = figure('Position',[600 300 850 200]);
 subplot(1,2,1)
-line([405 405],[0 1],'LineWidth',2); hold on
-line([488 488],[0 1],'LineWidth',2);
-line([561 561],[0 1],'LineWidth',2);
-line([638 638],[0 1],'LineWidth',2);
+line([405 405],[0 1],'LineWidth',2,'Color',col_405); hold on
+line([488 488],[0 1],'LineWidth',2,'Color',col_488);
+line([561 561],[0 1],'LineWidth',2,'Color',col_561);
+line([638 638],[0 1],'LineWidth',2,'Color',col_638);
 plot(T_qdot655.wavelength,T_qdot655.ex/0.59,'Color',col_red,'LineWidth',2);
 plot(T_exfilter.wavelength,T_exfilter.transmission,'k');
 plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',col_lightgray);
 xlim([350 750]); ylim([0 1]); xticks(300:50:800)
-xlabel('Wavelength (nm)')
-set(gca,'FontSize',fontsize)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'Layer','top')
 
 subplot(1,2,2)
 plot(T_qdot655.wavelength,T_qdot655.em,'Color',col_red,'LineWidth',2); hold on
@@ -91,10 +96,72 @@ plot(T_emfilter.wavelength,T_emfilter.transmission,'k');
 plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',col_lightgray);
 plot(T_qe.wavelength,T_qe.qe,'--k');
 xlim([350 750]); ylim([0 1]); xticks(300:50:800)
-xlabel('Wavelength (nm)')
-set(gca,'FontSize',fontsize)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'Layer','top')
 
 savefig(fig,fullfile(directory_out,'experimentSpectra_QDot655_lightMode.fig'))
 exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot655_lightMode.png'),'Resolution',400)
 set(gcf,'renderer','Painters')
 exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot655_lightMode.eps'))
+
+
+%% Plot data dark mode
+
+fig = figure('Position',[600 300 850 200]);
+set(gcf,'Color','k')
+
+subplot(1,2,1)
+line([405 405],[0 1],'LineWidth',2,'Color',col_405); hold on
+line([488 488],[0 1],'LineWidth',2,'Color',col_488);
+line([561 561],[0 1],'LineWidth',2,'Color',col_561);
+line([638 638],[0 1],'LineWidth',2,'Color',col_638);
+plot(T_qdot585.wavelength,T_qdot585.ex/0.48,'Color',col_orange,'LineWidth',2);
+plot(T_exfilter.wavelength,T_exfilter.transmission,'w');
+plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',0.4*col_lightgray);
+xlim([350 750]); ylim([0 1]); xticks(300:50:800)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'XColor','w','YColor','w','Color','k','Layer','top')
+
+subplot(1,2,2)
+plot(T_qdot585.wavelength,T_qdot585.em,'Color',col_orange,'LineWidth',2); hold on
+plot(T_emfilter.wavelength,T_emfilter.transmission,'w');
+plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',0.4*col_lightgray);
+plot(T_qe.wavelength,T_qe.qe,'--w');
+xlim([350 750]); ylim([0 1]); xticks(300:50:800)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'XColor','w','YColor','w','Color','k','Layer','top')
+
+savefig(fig,fullfile(directory_out,'experimentSpectra_QDot585_darkMode.fig'))
+exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot585_darkMode.png'),'Resolution',400)
+set(gcf,'renderer','Painters')
+exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot585_darkMode.eps'))
+
+
+fig = figure('Position',[600 300 850 200]);
+set(gcf,'Color','k')
+
+subplot(1,2,1)
+line([405 405],[0 1],'LineWidth',2,'Color',col_405); hold on
+line([488 488],[0 1],'LineWidth',2,'Color',col_488);
+line([561 561],[0 1],'LineWidth',2,'Color',col_561);
+line([638 638],[0 1],'LineWidth',2,'Color',col_638);
+plot(T_qdot655.wavelength,T_qdot655.ex/0.59,'Color',col_red,'LineWidth',2);
+plot(T_exfilter.wavelength,T_exfilter.transmission,'w');
+plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',0.4*col_lightgray);
+xlim([350 750]); ylim([0 1]); xticks(300:50:800)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'XColor','w','YColor','w','Color','k','Layer','top')
+
+subplot(1,2,2)
+plot(T_qdot655.wavelength,T_qdot655.em,'Color',col_red,'LineWidth',2); hold on
+plot(T_emfilter.wavelength,T_emfilter.transmission,'w');
+plot(T_dichroic.wavelength,T_dichroic.transmission,'Color',0.4*col_lightgray);
+plot(T_qe.wavelength,T_qe.qe,'--w');
+xlim([350 750]); ylim([0 1]); xticks(300:50:800)
+xlabel('Wavelength (nm)'); box off
+set(gca,'FontSize',fontsize,'XColor','w','YColor','w','Color','k','Layer','top')
+
+savefig(fig,fullfile(directory_out,'experimentSpectra_QDot655_darkMode.fig'))
+exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot655_darkMode.png'),'Resolution',400)
+set(gcf,'renderer','Painters')
+exportgraphics(fig,fullfile(directory_out,'experimentSpectra_QDot655_darkMode.eps'))
